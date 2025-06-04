16e674422144d7e605b5d8b4c66d85dc957f5234
gustinedarko <gustine369@gmail.com>
2025-06-04 05:25:38 -0700

    Header completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,8 @@ import { Facebook, Instagram, Linkedin } from "lucide-react";
 function Footer(){
     return (
         <footer className="bg-navy-blue text-white">
-            <h1 className="flex justify-center text-heading py-3"><span className="font-bold">Event</span> <span className="text-primary font-bold"> Hive</span></h1>
+            <h1 className="flex justify-center text-heading py-3"><span className="font-bold">Event</span>
+            <span className="text-primary font-bold"> Hive</span></h1>
             <form className="flex justify-center gap-4">
                 <input className="bg-white rounded-md text-black px-4" type="email" placeholder="Enter your mail" name="email" />
                 <button className="bg-primary rounded-md py-2 px-4" type="submit">Subscribe</button>
